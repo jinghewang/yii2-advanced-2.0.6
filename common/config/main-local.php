5a49c3f2e3100c9c66e34b67f40b0beec7ca1a31
jinghewang <jinghewang@163.com>
2016-01-04 13:57:09 +0800

    url country

---
+++ common/config/main-local.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    'components' => [
+        'language' => 'zh-CN', // 启用国际化支持
+        'sourceLanguage' => 'zh-CN', // 源代码采用中文
+        'timeZone' => 'Asia/Shanghai', // 设置时区
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'username' => 'root',
+            'password' => '123456',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,//隐藏index.php
+            //'enableStrictParsing' => false,
+            //'suffix' => '.html',//后缀，如果设置了此项，那么浏览器地址栏就必须带上.html后缀，否则会报404错误
+            'rules' => [
+                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+            ],
+        ],
+    ],
+];
