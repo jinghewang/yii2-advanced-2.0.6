49d8bd9cb2e969fbee999ee56bafc349d11d0ba3
jinghewang <jinghewang@163.com>
2016-01-04 16:57:24 +0800

    index images

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -4,14 +4,55 @@
 
 $this->title = 'Backend--Electronic Contract';
 ?>
+
+<style>
+    .carousel-inner .item img{
+        width: 100%;
+        height:300px;
+    }
+</style>
+
 <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="http://img4.duitang.com/uploads/blog/201403/20/20140320160303_rjFdG.thumb.600_0.jpeg" alt="...">
+                <div class="carousel-caption">
+                    美丽海南
+                </div>
+            </div>
+            <div class="item">
+                <img src="http://www.16sucai.com/d/file/2011/1018/20111018021807600.jpg" alt="...">
+                <div class="carousel-caption">
+                    美丽海南2
+                </div>
+            </div>
+            <div class="item">
+                <img src="http://www.qhlly.com/files/2013-1/20130122102154170965.jpg" alt="...">
+                <div class="carousel-caption">
+                    美丽海南3
+                </div>
+            </div>
+        </div>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
 
     <div class="body-content">
